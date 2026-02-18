ff9ef1ab496aacf709648c37414dc77ff7203733
obadx <abdullahamlyossef@gmail.com>
2026-02-18 16:48:56 +0200

    add more instructions

--- src/quran_muaalem/engine/main.py
+++ src/quran_muaalem/engine/main.py
@@ -1,7 +1,10 @@
-""""
+"""
 Create preprocessing deployment that extracts each features form every input
 
 Create Model Deplymnet its only responlbility to run model infrence with dynmanic batching and return logits wiche runs on the GPU
 
-Creat PostProcessing Deplyment that outputs only the text for every level without using the formating or alilgment 
-""""
+Creat PostProcessing Deplyment that outputs only the text for `phonemes` level and the ids for the rest of level as a dict
+without using aligment modules of the muaalem
+
+Every request comes to the preprocessing then bathces accumlates and the model run inference then post processing per evvery output
+"""
