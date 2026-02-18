95743b2e8647a401fbe40a33b082877b7ac14dd5
obadx <abdullahamlyossef@gmail.com>
2026-02-18 16:23:26 +0200

    add instructions

---
+++ src/quran_muaalem/engine/main.py
@@ -0,0 +1,7 @@
+""""
+Create preprocessing deployment that extracts each features form every input
+
+Create Model Deplymnet its only responlbility to run model infrence with dynmanic batching and return logits wiche runs on the GPU
+
+Creat PostProcessing Deplyment that outputs only the text for every level without using the formating or alilgment 
+""""
